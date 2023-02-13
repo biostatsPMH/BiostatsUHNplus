206200d4a2ab76ee9a64cb18726e6d27f65e1e3c
Pittman <Tyler.Pittman@uhn.ca>
2023-02-13 15:53:33 -0500

    Added Katrina's as.numeric.parse function

---
+++ R/as.numeric.parse.R
@@ -0,0 +1,30 @@
+#' Wrapper for the as.numeric function that prints entries that 
+#' fail to parse 
+#' 
+#' @param x string or vector to coerce to numeric
+#' @keywords as.numeric
+#' @export
+#' @examples
+#' z <- as.numeric.parse(c(1:5, "String1",6:10,"String2"))
+#' z
+as.numeric.parse <- function(x, ...){
+  # Arguments:
+  # x = string or vector to coerce to numeric
+  # ... = additional arguments passed to as.numeric()
+  
+  if (!class(x) %in% c("logical","numeric","double","integer","character","Date")) {
+    stop("Invalid data type")
+  }
+  
+  y <- suppressWarnings(as.numeric(x, ...))
+  noparse <- x[!is.na(x) & is.na(y)]
+  
+  if (length(noparse) > 0) {
+    noparse_warn <- paste0("Entry ", which(!is.na(x) & is.na(y)), ", '", noparse, "'")
+    message("The following entries were converted to NA values:")
+    for (i in 1:length(noparse_warn)){
+      message(noparse_warn[i])
+    }
+  }
+  return(y)
+}
