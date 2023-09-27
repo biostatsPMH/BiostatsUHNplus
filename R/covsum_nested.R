92ebfa7648e5c0838ece765661772c57fb23c1f6
tylerpittman <pittman.tyler@gmail.com>
2023-09-27 11:04:39 -0400

    Added .drop=FALSE to group_by in maincovCovsIdData1()

--- R/covsum_nested.R
+++ R/covsum_nested.R
@@ -111,7 +111,7 @@ covsum_nested <- function (data, covs, maincov = NULL, id = NULL, digits = 1, nu
       dplyr::select(!!!(rlang::syms(maincov)), !!!(rlang::syms(covs)), !!!(rlang::syms(id))) |>
       dplyr::group_by(!!!(rlang::syms(id))) |>
       dplyr::reframe(dplyr::across(where(is.numeric), ~ mean(.x, na.rm = TRUE)), dplyr::across(where(is.date), ~ mean(.x, na.rm = TRUE)), dplyr::across(where(is.factor), ~ modeest::mlv(.x, method = mfv))) |>
-      dplyr::group_by(!!!(rlang::syms(id))) |>
+      dplyr::group_by(!!!(rlang::syms(id)), .drop=FALSE) |>
       dplyr::filter(dplyr::row_number() == ceiling(n()/2))
     tto <- as.data.frame(tto)
     tto
