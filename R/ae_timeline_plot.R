9e0aa29da3afd2df7a63a16305730a88eda7f255
tylerpittman <pittman.tyler@gmail.com>
2023-11-22 16:17:49 -0500

    update

--- R/ae_timeline_plot.R
+++ R/ae_timeline_plot.R
@@ -53,6 +53,7 @@
 #' @importFrom cowplot get_legend
 #' @importFrom ggstance geom_pointrangeh
 #' @import ggplot2
+#' @import lifecycle
 #' @export
 #' @examples
 #' data("drug1_admin", "drug2_admin", "ae");
