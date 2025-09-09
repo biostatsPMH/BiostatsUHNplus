8c637669fa4e5b307be3959fea8f64cb3af39076
tylerpittman <pittman.tyler@gmail.com>
2025-09-09 12:35:33 -0400

    update in progress

--- R/dsmb_ddp.R
+++ R/dsmb_ddp.R
@@ -95,6 +95,9 @@ dsmb_ddp <- function(protocol,setwd,title,comp=NULL,pi,presDate,cutDate,boundDat
   if (is.null(ae_attribVarText)) {
     ae_attribVarText <- c("Definite", "Probable", "Possible");
   }
+  if (!is.null(numSubj)) {
+    numSubj <- c(sum(numSubj), numSubj)
+  }
   if (is.null(comp)) {
     subjectsKeep_DF <- plyr::join_all(baseline_datasets, by = subjID, type = "full") |>
       #### --------------------------------------------- ####
@@ -121,6 +124,9 @@ dsmb_ddp <- function(protocol,setwd,title,comp=NULL,pi,presDate,cutDate,boundDat
       dplyr::filter(!PT_ELIG_IND_3 %in% ineligVarText, !Subject %in% subjID_ineligText) |>
       dplyr::arrange(Subject) |>
       dplyr::filter(!is.na(comp))
+    subjectsKeep_DFa <- subjectsKeep_DF
+    subjectsKeep_DFa$comp = ""
+    subjectsKeep_DF <- rbind(subjectsKeep_DFa, subjectsKeep_DF)
   }
   
   
@@ -263,153 +269,168 @@ dsmb_ddp <- function(protocol,setwd,title,comp=NULL,pi,presDate,cutDate,boundDat
   
   # Rel AEs by comp;
   sheetNamesR <- NULL;
-  #i <- 1;
-  for (i in 1:length(unique(subjectsKeep_DF[["comp"]]))) {
-    comp <- unique(subjectsKeep_DF[["comp"]])[i]
-    tryCatch({
-      subjects_DF <- subjectsKeep_DF[which(subjectsKeep_DF$comp %in% c(comp)), ] 
-    }, error=function(e){})
-    
-    aeKeep_DF <- ae_dataset |>
-      #### --------------------------------------------- ####
-    #### Just modify the below line for variable names ####
-    dplyr::mutate(Subject = eval(parse(text=subjID)), ae_grade_code_dyn_std = eval(parse(text=ae_severityVar)), CTCAE5_LLT_NM = eval(parse(text=ae_detailOtherVar)), AE_VERBATIM_TRM_TXT = eval(parse(text=ae_verbatimVar)), AE_ONSET_DT_INT = eval(parse(text=ae_onsetDtVar)), ae_detail = eval(parse(text=ae_detailVar)), ae_category = eval(parse(text=ae_categoryVar))) |>
-      dplyr::select(Subject, ae_grade_code_dyn_std, CTCAE5_LLT_NM, AE_VERBATIM_TRM_TXT, AE_ONSET_DT_INT, ae_detail, ae_category) |>
-      #### --------------------------------------------- ####
-    dplyr::mutate(ae_detail = toupper(ifelse(stringr::str_detect(ae_detail, ae_detailOtherText), trimws(AE_VERBATIM_TRM_TXT), ae_detail)), AE_ONSET_DT_INT = toupper(format(as.Date(AE_ONSET_DT_INT, tz = "UTC"), "%d%b%Y")), ae_category = toupper(ae_category)) |>
-      dplyr::mutate(ae_detail = toupper(ifelse(is.na(ae_detail), CTCAE5_LLT_NM, ae_detail))) |>
-      dplyr::filter(dplyr::if_any(dplyr::all_of(ae_attribVars), ~ . %in% ae_attribVarText)) |>
-      dplyr::arrange(Subject)
-    
-    aes1_DF <- subjects_DF |> 
-      dplyr::left_join(aeKeep_DF, by = "Subject") |>
-      dplyr::select(Subject, AE_ONSET_DT_INT, ae_detail, ae_category, ae_grade_code_dyn_std, PARTIC_ENROL_DT_INT, CTCAE5_LLT_NM, AE_VERBATIM_TRM_TXT) |>
-      dplyr::arrange(Subject) |>
-      #dplyr::filter(as.Date(PARTIC_ENROL_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y"), as.Date(AE_ONSET_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y"), !is.na(ae_detail)) |>
-      dplyr::filter(is.na(PARTIC_ENROL_DT_INT) | as.Date(PARTIC_ENROL_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y")) |>
-      dplyr::filter(is.na(AE_ONSET_DT_INT) | as.Date(AE_ONSET_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y")) |>
-      dplyr::distinct(Subject, AE_ONSET_DT_INT, ae_detail, ae_category, ae_grade_code_dyn_std, PARTIC_ENROL_DT_INT) |>
-      dplyr::filter(!ae_detail == "") |>
-      dplyr::filter(!ae_category == "") |>
-      #dplyr::filter(!ae_grade_code_dyn_std == "") |>
-      dplyr::mutate(ae_grade_code_dyn_std = as.numeric(gsub("[^0-9.-]", "", ae_grade_code_dyn_std))) 
-    
-    total_subj_count <- length(unique(subjects_DF[which(!is.na(subjects_DF$gender_code)), ]$Subject));
-    if (!is.null(numSubj)) {
-      total_subj_count <- numSubj[i];
-    }
-    
-    total_ae_count <- length(aes1_DF$ae_category);
-    
-    #### Table;
-    aes1_DF <- aes1_DF |>
-      dplyr::mutate(AE_Grade = ae_grade_code_dyn_std, AE_Detail = ae_detail, AE_Class = ae_category, Study = paste(protocol, sep=""))
-    
-    # Below takes worst AE for subject by AE detail for each study;
-    all_AEs_subject <- aes1_DF %>% 
-      dplyr::select(AE_Grade, AE_Detail, Study, Subject) %>%
-      dplyr::group_by(Study, AE_Detail, AE_Grade, Subject) %>%   
-      dplyr::arrange(Study, Subject, AE_Detail, desc(AE_Grade)) %>%
-      dplyr::group_by(Study, Subject, AE_Detail) %>% 
-      dplyr::filter(row_number()==1) #takes the top (highest grade) per subject per study
-    
-    # Get number of distinct subjects per AE grade
-    distinct_counts <- all_AEs_subject |>
-      group_by(AE_Grade) |>
-      summarise(n_distinct_subjects = n_distinct(Subject)) |>
-      pull(n_distinct_subjects)
-    
-    totalAESubj <- all_AEs_subject |>
-      group_by(Study) |>
-      summarise(n_distinct_subjects = n_distinct(Subject)) |>
-      pull(n_distinct_subjects)
-    
-    #total_subj_count;
-    
-    col_AE_totals <- c(totalAESubj, distinct_counts)
-    
-    col_totals <- c(rep(total_subj_count, each = 6))
-    
-    levelsStrat <- sort(c("1", "2", "3", "4", "5"));
-    all_AEs_subject$AE_Grade <- factor(all_AEs_subject$AE_Grade, levels = levelsStrat);
-    tab4 <- BiostatsUHNplus:::covsum_nested(data = all_AEs_subject, id = c("Subject", "AE_Detail"), covs = c("AE_Detail"),  maincov = "AE_Grade", testcat = "Fisher", percentage = c("column"), show.tests = F, pvalue = F, effSize = F, full = T, IQR = F, digits = 1, digits.cat = 1, sanitize = FALSE, dropLevels = FALSE, nicenames = TRUE);
-    
-    # Should work to sort in descending order;
-    first_row <- tab4[1, ];
-    rest_sorted <- tab4[-1, ][order(-as.numeric(sapply(strsplit(tab4[-1, 2], " "), `[`, 1))), ];
-    tab4  <- rbind(first_row, rest_sorted);  
-    
-    # Remove all text in parentheses from character columns
-    tab4[] <- lapply(tab4, function(x) {
-      if (is.character(x)) {
-        gsub("\\s*\\([^\\)]+\\)", "", x)
-      } else {
-        x
-      }
-    })
-    
-    # Remove text in parentheses from column names
-    names(tab4) <- gsub("\\s*\\([^\\)]+\\)", "", names(tab4))
-    
-    # Remove the first row
-    fr_tab4 <- tab4[1, ]
-    tab4 <- tab4[-1, ]
-    
-    # Convert to numeric
-    tab4[, -1] <- lapply(tab4[, -1], as.numeric)
-    
-    # Append totals to column names
-    names(fr_tab4) <- c("Covariate", paste0(names(fr_tab4[-1]), " (n=", col_AE_totals, ")")) 
-    
-    # Create a new dataframe with counts and percentages
-    tab4_percent <- as.data.frame(mapply(function(col, total) {
-      paste0(col, " (", sprintf("%.1f", 100 * col / total), ")")
-    }, tab4[,-1], col_totals, SIMPLIFY = FALSE))
-    
-    tab4_percent <- cbind(tab4[, 1], tab4_percent)
-    names(tab4_percent) <- names(fr_tab4) 
-    tab4 <- rbind(fr_tab4, tab4_percent)
-    #tab4 <- tab4_percent
-    
-    to_indent <- which(tab4$Covariate %in% grep("^~~~", tab4$Covariate, value = TRUE))
-    to_bold_name <- which(!tab4$Covariate %in% grep("^~~~", tab4$Covariate, value = TRUE))
-    bold_cells <- arrayInd(to_bold_name, dim(tab4))
-    
-    #tab4$Covariate[1] <- gsub("\\\\textbf\\{", "", tab4$Covariate[1])
-    #tab4$Covariate[1] <- substr(tab4$Covariate[1], 1, nchar(tab4$Covariate[1]) - 1)
-    tab4$Covariate[1] <- "MedDRA Lowest Level Term"
-    tab4$Covariate <- gsub("^~~~", "", tab4$Covariate)
-    
-    tmp <- cbind(tab4[,c(1,3:7)], tab4[,c(2)])
-    tmp <-  tmp[-1, ]
-    cn1 <- "MedDRA Lowest Level Term"
-    cn2 <- paste0("# of patients Grade 1", " (n=", col_AE_totals[2], ")")
-    cn3 <- paste0("# of patients Grade 2", " (n=", col_AE_totals[3], ")")
-    cn4 <- paste0("# of patients Grade 3", " (n=", col_AE_totals[4], ")")
-    cn5 <- paste0("# of patients Grade 4", " (n=", col_AE_totals[5], ")")
-    cn6 <- paste0("# of patients Grade 5", " (n=", col_AE_totals[6], ")")
-    cn7 <- paste0("Total # of patients", " (n=", col_AE_totals[1], " out of ", total_subj_count, ")")
-    
-    colnames(tmp) <- c(cn1, cn2, cn3, cn4, cn5, cn6, cn7)
-    tmpTN <- paste("Rel_AEs_", comp, sep="");
-    tmpTN <- gsub(" ", "_", tmpTN);
-    
-    assign(tmpTN, tmp);
-    sheetNamesR[i] <- tmpTN;
-  } 
-  
-  
   if (related_ae == TRUE) {
-    aeKeep_DF <- aeKeep_DF |>
-      dplyr::filter(dplyr::if_any(dplyr::all_of(ae_attribVars), ~ . %in% ae_attribVarText))
+    #i <- 1;
+    for (i in 1:length(unique(subjectsKeep_DF[["comp"]]))) {
+      comp <- unique(subjectsKeep_DF[["comp"]])[i]
+      tryCatch({
+        subjects_DF <- subjectsKeep_DF[which(subjectsKeep_DF$comp %in% c(comp)), ] 
+      }, error=function(e){})
+      
+      aeKeep_DF <- ae_dataset |>
+        #### --------------------------------------------- ####
+      #### Just modify the below line for variable names ####
+      dplyr::mutate(Subject = eval(parse(text=subjID)), ae_grade_code_dyn_std = eval(parse(text=ae_severityVar)), CTCAE5_LLT_NM = eval(parse(text=ae_detailOtherVar)), AE_VERBATIM_TRM_TXT = eval(parse(text=ae_verbatimVar)), AE_ONSET_DT_INT = eval(parse(text=ae_onsetDtVar)), ae_detail = eval(parse(text=ae_detailVar)), ae_category = eval(parse(text=ae_categoryVar))) |>
+        dplyr::select(Subject, ae_grade_code_dyn_std,  dplyr::all_of(ae_attribVars), CTCAE5_LLT_NM, AE_VERBATIM_TRM_TXT, AE_ONSET_DT_INT, ae_detail, ae_category) |>
+        #### --------------------------------------------- ####
+      dplyr::mutate(ae_detail = toupper(ifelse(stringr::str_detect(ae_detail, ae_detailOtherText), trimws(AE_VERBATIM_TRM_TXT), ae_detail)), AE_ONSET_DT_INT = toupper(format(as.Date(AE_ONSET_DT_INT, tz = "UTC"), "%d%b%Y")), ae_category = toupper(ae_category)) |>
+        dplyr::mutate(ae_detail = toupper(ifelse(is.na(ae_detail), CTCAE5_LLT_NM, ae_detail))) |>
+        dplyr::arrange(Subject)
+      
+      aeKeep_DF <- aeKeep_DF |>
+        dplyr::filter(dplyr::if_any(dplyr::all_of(ae_attribVars), ~ . %in% ae_attribVarText))
+      
+      aes1_DF <- subjects_DF |> 
+        dplyr::left_join(aeKeep_DF, by = "Subject") |>
+        dplyr::select(Subject, AE_ONSET_DT_INT, ae_detail, ae_category, ae_grade_code_dyn_std, PARTIC_ENROL_DT_INT, CTCAE5_LLT_NM, AE_VERBATIM_TRM_TXT) |>
+        dplyr::arrange(Subject) |>
+        #dplyr::filter(as.Date(PARTIC_ENROL_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y"), as.Date(AE_ONSET_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y"), !is.na(ae_detail)) |>
+        dplyr::filter(is.na(PARTIC_ENROL_DT_INT) | as.Date(PARTIC_ENROL_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y")) |>
+        dplyr::filter(is.na(AE_ONSET_DT_INT) | as.Date(AE_ONSET_DT_INT, "%d%b%Y") <= as.Date(cutDate, "%d%b%Y")) |>
+        dplyr::distinct(Subject, AE_ONSET_DT_INT, ae_detail, ae_category, ae_grade_code_dyn_std, PARTIC_ENROL_DT_INT) |>
+        dplyr::filter(!ae_detail == "") |>
+        dplyr::filter(!ae_category == "") |>
+        #dplyr::filter(!ae_grade_code_dyn_std == "") |>
+        dplyr::mutate(ae_grade_code_dyn_std = as.numeric(gsub("[^0-9.-]", "", ae_grade_code_dyn_std))) 
+      
+      total_subj_count <- length(unique(subjects_DF[which(!is.na(subjects_DF$gender_code)), ]$Subject));
+      if (!is.null(numSubj)) {
+        total_subj_count <- numSubj[i];
+      }
+      
+      total_ae_count <- length(aes1_DF$ae_category);
+      
+      #### Table;
+      aes1_DF <- aes1_DF |>
+        dplyr::mutate(AE_Grade = ae_grade_code_dyn_std, AE_Detail = ae_detail, AE_Class = ae_category, Study = paste(protocol, sep=""))
+      
+      # Below takes worst AE for subject by AE detail for each study;
+      all_AEs_subject <- aes1_DF %>% 
+        dplyr::select(AE_Grade, AE_Detail, Study, Subject) %>%
+        dplyr::group_by(Study, AE_Detail, AE_Grade, Subject) %>%   
+        dplyr::arrange(Study, Subject, AE_Detail, desc(AE_Grade)) %>%
+        dplyr::group_by(Study, Subject, AE_Detail) %>% 
+        dplyr::filter(row_number()==1) #takes the top (highest grade) per subject per study
+      
+      # Get number of distinct subjects per AE grade
+      distinct_counts <- all_AEs_subject |>
+        group_by(AE_Grade) |>
+        summarise(n_distinct_subjects = n_distinct(Subject)) |>
+        pull(n_distinct_subjects)
+      
+      totalAESubj <- all_AEs_subject |>
+        group_by(Study) |>
+        summarise(n_distinct_subjects = n_distinct(Subject)) |>
+        pull(n_distinct_subjects)
+      
+      #total_subj_count;
+      
+      col_AE_totals <- c(totalAESubj, distinct_counts)
+      
+      col_totals <- c(rep(total_subj_count, each = 6))
+      
+      levelsStrat <- sort(c("1", "2", "3", "4", "5"));
+      all_AEs_subject$AE_Grade <- factor(all_AEs_subject$AE_Grade, levels = levelsStrat);
+      tab4 <- BiostatsUHNplus:::covsum_nested(data = all_AEs_subject, id = c("Subject", "AE_Detail"), covs = c("AE_Detail"),  maincov = "AE_Grade", testcat = "Fisher", percentage = c("column"), show.tests = F, pvalue = F, effSize = F, full = T, IQR = F, digits = 1, digits.cat = 1, sanitize = FALSE, dropLevels = FALSE, nicenames = TRUE);
+      
+      # Should work to sort in descending order;
+      first_row <- tab4[1, ];
+      rest_sorted <- tab4[-1, ][order(-as.numeric(sapply(strsplit(tab4[-1, 2], " "), `[`, 1))), ];
+      tab4  <- rbind(first_row, rest_sorted);  
+      
+      # Remove all text in parentheses from character columns
+      tab4[] <- lapply(tab4, function(x) {
+        if (is.character(x)) {
+          gsub("\\s*\\([^\\)]+\\)", "", x)
+        } else {
+          x
+        }
+      })
+      
+      # Remove text in parentheses from column names
+      names(tab4) <- gsub("\\s*\\([^\\)]+\\)", "", names(tab4))
+      
+      # Remove the first row
+      fr_tab4 <- tab4[1, ]
+      tab4 <- tab4[-1, ]
+      
+      # Convert to numeric
+      tab4[, -1] <- lapply(tab4[, -1], as.numeric)
+      
+      # Append totals to column names
+      names(fr_tab4) <- c("Covariate", paste0(names(fr_tab4[-1]), " (n=", col_AE_totals, ")")) 
+      
+      # Create a new dataframe with counts and percentages
+      tab4_percent <- as.data.frame(mapply(function(col, total) {
+        paste0(col, " (", sprintf("%.1f", 100 * col / total), ")")
+      }, tab4[,-1], col_totals, SIMPLIFY = FALSE))
+      
+      tab4_percent <- cbind(tab4[, 1], tab4_percent)
+      names(tab4_percent) <- names(fr_tab4) 
+      tab4 <- rbind(fr_tab4, tab4_percent)
+      #tab4 <- tab4_percent
+      
+      to_indent <- which(tab4$Covariate %in% grep("^~~~", tab4$Covariate, value = TRUE))
+      to_bold_name <- which(!tab4$Covariate %in% grep("^~~~", tab4$Covariate, value = TRUE))
+      bold_cells <- arrayInd(to_bold_name, dim(tab4))
+      
+      #tab4$Covariate[1] <- gsub("\\\\textbf\\{", "", tab4$Covariate[1])
+      #tab4$Covariate[1] <- substr(tab4$Covariate[1], 1, nchar(tab4$Covariate[1]) - 1)
+      tab4$Covariate[1] <- "MedDRA Lowest Level Term"
+      tab4$Covariate <- gsub("^~~~", "", tab4$Covariate)
+      
+      tmp <- cbind(tab4[,c(1,3:7)], tab4[,c(2)])
+      tmp <-  tmp[-1, ]
+      cn1 <- "MedDRA Lowest Level Term"
+      cn2 <- paste0("# of patients Grade 1", " (n=", col_AE_totals[2], ")")
+      cn3 <- paste0("# of patients Grade 2", " (n=", col_AE_totals[3], ")")
+      cn4 <- paste0("# of patients Grade 3", " (n=", col_AE_totals[4], ")")
+      cn5 <- paste0("# of patients Grade 4", " (n=", col_AE_totals[5], ")")
+      cn6 <- paste0("# of patients Grade 5", " (n=", col_AE_totals[6], ")")
+      cn7 <- paste0("Total # of patients", " (n=", col_AE_totals[1], " out of ", total_subj_count, ")")
+      
+      colnames(tmp) <- c(cn1, cn2, cn3, cn4, cn5, cn6, cn7)
+      tmpTN <- paste("Rel_AEs_", comp, sep="");
+      tmpTN <- gsub(" ", "_", tmpTN);
+      
+      assign(tmpTN, tmp);
+      sheetNamesR[i] <- tmpTN;
+    } 
   }
-    
-  names(list_of_datasets2) <- make.unique(names(list_of_datasets2), sep = '_')
-  openxlsx::write.xlsx(list_of_datasets2, paste(setwd, "\\", protocol, "_AEs_", 
-                                                  pullDate, ".xlsx", sep=""));
-    
-    
- 
+  
+  
+  # Combine sheet names so they alternate
+  sheetNames <- as.vector(rbind(sheetNamesA, sheetNamesR))
+  
+  list_of_datasets <- lapply(sheetNames, function(x) get(x, mode="list"));
+  names(list_of_datasets) <- c(sheetNames);
+  
+  
+  # Rename dataset, if it exists
+  old_name <- "All_AEs_"
+  new_name <- "All_AEs"
+  if (old_name %in% names(list_of_datasets)) {
+    names(list_of_datasets)[names(list_of_datasets) == old_name] <- new_name
+  }
+  old_name <- "Rel_AEs_"
+  new_name <- "Rel_AEs"
+  if (old_name %in% names(list_of_datasets)) {
+    names(list_of_datasets)[names(list_of_datasets) == old_name] <- new_name
+  }
+  
+  names(list_of_datasets) <- make.unique(names(list_of_datasets), sep = '_')
+  openxlsx::write.xlsx(list_of_datasets, paste(setwd, "\\", protocol, "_AEs_", 
+                                               presDate, ".xlsx", sep=""));
     
   #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#;
 }
